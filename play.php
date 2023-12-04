7e72d45e57a4f09b9eec6cea686d50c1cf4754b0
Darius Matulionis <darius.matulionis@supermetrics.com>
2023-12-04 12:29:48 +0200

    Game 3 & 4

--- play.php
+++ play.php
@@ -5,11 +5,15 @@ require __DIR__.'/vendor/autoload.php';
 
 use DarkaOnLine\AdventOfCode2023\Game1;
 use DarkaOnLine\AdventOfCode2023\Game2;
+use DarkaOnLine\AdventOfCode2023\Game3;
+use DarkaOnLine\AdventOfCode2023\Game4;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
 
 $application->add(new Game1);
-$application->add(new Game2());
+$application->add(new Game2);
+$application->add(new Game3);
+$application->add(new Game4);
 
 $application->run();
