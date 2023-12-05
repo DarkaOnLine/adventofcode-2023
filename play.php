55be0a9744816c04bd913d8774ffc0dde68402f5
Darius Matulionis <darius.matulionis@supermetrics.com>
2023-12-05 14:30:58 +0200

    Game 5

--- play.php
+++ play.php
@@ -7,6 +7,7 @@ use DarkaOnLine\AdventOfCode2023\Game1;
 use DarkaOnLine\AdventOfCode2023\Game2;
 use DarkaOnLine\AdventOfCode2023\Game3;
 use DarkaOnLine\AdventOfCode2023\Game4;
+use DarkaOnLine\AdventOfCode2023\Game5;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
@@ -15,5 +16,6 @@ $application->add(new Game1);
 $application->add(new Game2);
 $application->add(new Game3);
 $application->add(new Game4);
+$application->add(new Game5);
 
 $application->run();
